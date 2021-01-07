f7172afa340181346f6855660f6435844f7974e0
Theo Paris <theoparisdesigns@gmail.com>
2021-01-07 12:27:41 -0800

    - orangize file structure - switch to jinja j2 templates

--- run.py
+++ run.py
@@ -1,36 +1,46 @@
 import sanic
 from datetime import datetime
 from os import environ
+from jinja2 import Environment, PackageLoader, select_autoescape
+from firstclass_dotenv import Dotenv
+
+dotenv = Dotenv
+
+try:
+    dotenv.load()
+except:
+    print("Could not .env file, using os environment variables...")
+
+env = Environment(
+    loader=PackageLoader("./public"),
+    autoescape=select_autoescape(["html", "xml", "tpl"]),
+    enable_async=True,
+)
+
+
+async def template(tpl, **kwargs):
+    template = env.get_template(tpl)
+    content = await template.render_async(kwargs)
+    return response.html(content)
+
 
 now = datetime.now()
-app = sanic.app.Sanic(name='xDash')
+app: sanic.Sanic = sanic.app.Sanic(name="xDash")
 response = sanic.response
 cur_time_short = now.strftime("[%Y-%m-%d %H:%M:%S]")
+api_url = environ.get("API_URL", "http://localhost:6969/v1")
+app.static("/", "./public")
 
-@app.route('/')
+
+@app.route("/")
 def func(request):
-    return response.file('index.html')
+    return template("index.j2", api_url=api_url)
 
-@app.route('/login')
+
+@app.route("/login")
 def func(request):
-    return response.file('login.html')
+    return template("login.j2")
 
-
-@app.route('css/skeleton.css')
-def func(request):
-    return response.file('skeleton.css')
-
-@app.route('css/normalize.css')
-def func(request):
-    return response.file('normalize.css')
-
-@app.route('css/style.css')
-def func(request):
-    return response.file('style.css')
-
-@app.route('js/auth.js')
-def func(request):
-    return response.file('auth.js')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=environ.get("PORT", 8000))
